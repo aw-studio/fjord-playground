f612e8cc07c3e3def27a7fed38f3cf7e3be08758
Jannes Behrens <jannescb@me.com>
2020-02-29 19:41:03 +0100

    bugfix

--- resources/views/blocks/image.blade.php
+++ resources/views/blocks/image.blade.php
@@ -1,3 +1,5 @@
 <div class="col-12 col-md-6">
-    <img src="{{$block->image->getFullUrl('lg')}}" class="img-fluid">
+    @if($block->image)
+        <img src="{{$block->image->getFullUrl('lg')}}" class="img-fluid">
+    @endif
 </div>
